0ce099e2c402448e1437fad635d31c44b99cb945
hodayapinko <hodayapinko@gmail.com>
2026-07-03 19:39:58 +0300

    fix libary

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
-import rtlPlugin from 'stylis-plugin-rtl';
+import rtlPlugin from '@mui/stylis-plugin-rtl';
 import theme from './theme';
 import './index.css';
 import App from './App';
